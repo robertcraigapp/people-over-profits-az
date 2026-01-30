2915194d047affbb1e0a6aa4b78f032e17fd9720
mvhsbball22 <robert.craig.iii@gmail.com>
2026-01-30 13:19:14 -0600

    get react router working

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,19 +1,19 @@
 // src/App.tsx
 
-import popazLogo from "./assets/popaz-logo.png";
-import "./App.css";
+import popazLogo from './assets/People Over Profits Logo.png';
+import './App.css';
 
 function App() {
-	return (
-		<>
-			<div>
-				<img src={popazLogo} alt="temporary logo for POPAZ" />
-			</div>
-			<div>
-				<p>Stay tuned for changes in Arizona.</p>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div>
+                <img src={popazLogo} alt='temporary logo for POPAZ' />
+            </div>
+            <div>
+                <p>Stay tuned for changes in Arizona.</p>
+            </div>
+        </>
+    );
 }
 
 export default App;
